3c01744b2c70d23b6c9d2366f693ab6ab2b9c1f8
eleven <87019683+yoctosecond@users.noreply.github.com>
2021-09-04 18:47:07 -0300

    update do code mais limpo que eu consegui

--- projekt/nucleus2(tentando deixar o mais simples possivel).py
+++ projekt/nucleus2(tentando deixar o mais simples possivel).py
@@ -1,25 +1,23 @@
 import random as r
-
 answer, control = ".", False
 
 def roll():
-  dice = r.randrange(1, 7)
+  dice = r.randint(1, 7)
   return dice
 
 def make_1st_question():
   global answer, control
-  answer, control = input("Do you wanna play? ").lower(), True
+  answer, control = input("Do you wanna play? (Y/N) ").lower(), True
   verification()
 
 def make_2nd_question():
   global answer, control 
-  answer, control = input("Do you wanna play once more? ").lower(), False
+  answer, control = input("Do you wanna play once more? (Y/N) ").lower(), False
   verification()
 
 def verification():
   if answer == "yes" or answer == "y":
-    print(f"This is your number: {roll()}")
-    make_2nd_question()
+    print(f"This is your number: {roll()}"), make_2nd_question()
   elif answer == "no" or answer == "n":
     print("Baibai!")
   else:
