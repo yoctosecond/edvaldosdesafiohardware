26607ffe68b637e2e99a6b0677ad20d3fc698cfe
eleven <87019683+yoctosecond@users.noreply.github.com>
2021-09-04 19:01:14 -0300

    final update

--- projekt/nucleus2(tentando deixar o mais simples possivel).py
+++ projekt/nucleus2(tentando deixar o mais simples possivel).py
@@ -7,18 +7,18 @@ def roll():
 
 def make_1st_question():
   global answer, control
-  answer, control = input("Do you wanna play? (Y/N) ").lower(), True
+  answer, control = input("Do you wanna play? (Y/N) ").lower()[0:1], True
   verification()
 
 def make_2nd_question():
   global answer, control 
-  answer, control = input("Do you wanna play once more? (Y/N) ").lower(), False
+  answer, control = input("Do you wanna play once more? (Y/N) ").lower()[0:1], False
   verification()
 
 def verification():
-  if answer == "yes" or answer == "y":
+  if answer == "y":
     print(f"This is your number: {roll()}"), make_2nd_question()
-  elif answer == "no" or answer == "n":
+  elif answer == "n":
     print("Baibai!")
   else:
     print("You made a typo! Type again slowly and calmly.")
